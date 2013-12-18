da647def4c432dd48fb1a1cf35549757b61cffbc
beepscore <support@beepscore.com>
2013-12-18 15:50:23 -0800

    Add CPDScatterPlotViewController

---
+++ CorePlotDemo/CPDScatterPlotViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPDScatterPlotViewController.h
+//  CorePlotDemo
+//
+//  Created by Steve Baker on 12/18/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPDScatterPlotViewController : UIViewController
+
+@end
