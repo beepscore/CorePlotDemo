7373464d4c2f243333e50683b9e3daf932f5bc51
beepscore <support@beepscore.com>
2013-12-19 14:21:51 -0800

    Implement configureGraph. Now scatter plot view shows dark background with axes. Can pinch to scale and zoom, and can pan.

--- CorePlotDemo/CPDScatterPlotViewController.m
+++ CorePlotDemo/CPDScatterPlotViewController.m
@@ -57,6 +57,31 @@
 }
 
 -(void)configureGraph {
+    // 1 - Create the graph
+    CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
+    [graph applyTheme:[CPTTheme themeNamed:kCPTDarkGradientTheme]];
+    self.hostView.hostedGraph = graph;
+
+    // 2 - Set graph title
+    NSString *title = @"Portfolio Prices: April 2012";
+    graph.title = title;
+
+    // 3 - Create and set text style
+    CPTMutableTextStyle *titleStyle = [CPTMutableTextStyle textStyle];
+    titleStyle.color = [CPTColor whiteColor];
+    titleStyle.fontName = @"Helvetica-Bold";
+    titleStyle.fontSize = 16.0f;
+    graph.titleTextStyle = titleStyle;
+    graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
+    graph.titleDisplacement = CGPointMake(0.0f, 10.0f);
+
+    // 4 - Set padding for plot area
+    [graph.plotAreaFrame setPaddingLeft:30.0f];
+    [graph.plotAreaFrame setPaddingBottom:30.0f];
+    
+    // 5 - Enable user interactions for plot space
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
+    plotSpace.allowsUserInteraction = YES;
 }
 
 -(void)configurePlots {
