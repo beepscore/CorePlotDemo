f8501783acda85e46b368eb754d984fe13c58e39
beepscore <support@beepscore.com>
2013-12-19 00:56:12 -0800

    In CPDBarGraphViewController.m add properties and method stubs.

--- CorePlotDemo/CPDBarGraphViewController.m
+++ CorePlotDemo/CPDBarGraphViewController.m
@@ -12,20 +12,64 @@
 
 @interface CPDBarGraphViewController ()
 
+@property (nonatomic, strong) IBOutlet CPTGraphHostingView *hostView;
+@property (nonatomic, strong) CPTBarPlot *aaplPlot;
+@property (nonatomic, strong) CPTBarPlot *googPlot;
+@property (nonatomic, strong) CPTBarPlot *msftPlot;
+@property (nonatomic, strong) CPTPlotSpaceAnnotation *priceAnnotation;
+
+-(IBAction)aaplSwitched:(id)sender;
+-(IBAction)googSwitched:(id)sender;
+-(IBAction)msftSwitched:(id)sender;
+
 @end
 
 @implementation CPDBarGraphViewController
+
+CGFloat const CPDBarWidth = 0.25f;
+CGFloat const CPDBarInitialX = 0.25f;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self initPlot];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Chart behavior
+-(void)initPlot {
+    self.hostView.allowPinchScaling = NO;
+    [self configureGraph];
+    [self configurePlots];
+    [self configureAxes];
+}
+
+-(void)configureGraph {
+}
+
+-(void)configurePlots {
+}
+
+-(void)configureAxes {
+}
+
+-(void)hideAnnotation:(CPTGraph *)graph {
+}
+
+#pragma mark - IBActions
+-(IBAction)aaplSwitched:(id)sender {
+}
+
+-(IBAction)googSwitched:(id)sender {
+}
+
+-(IBAction)msftSwitched:(id)sender {
 }
 
 #pragma mark - CPTPlotDataSource methods
