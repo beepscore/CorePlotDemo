8aa6a856bd43869b403cf6284caebf4792b7031f
beepscore <support@beepscore.com>
2013-12-19 01:29:16 -0800

    Implement configureGraph

--- CorePlotDemo/CPDBarGraphViewController.m
+++ CorePlotDemo/CPDBarGraphViewController.m
@@ -92,6 +92,40 @@ CGFloat const CPDBarInitialX = 0.25f;
 }
 
 -(void)configureGraph {
+    
+    // 1 - Create the graph
+    CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
+    graph.plotAreaFrame.masksToBorder = NO;
+    self.hostView.hostedGraph = graph;
+    
+    // 2 - Configure the graph
+    [graph applyTheme:[CPTTheme themeNamed:kCPTPlainBlackTheme]];
+    graph.paddingBottom = 30.0f;
+    graph.paddingLeft  = 30.0f;
+    graph.paddingTop    = -1.0f;
+    graph.paddingRight  = -5.0f;
+    
+    // 3 - Set up styles
+    CPTMutableTextStyle *titleStyle = [CPTMutableTextStyle textStyle];
+    titleStyle.color = [CPTColor whiteColor];
+    titleStyle.fontName = @"Helvetica-Bold";
+    titleStyle.fontSize = 16.0f;
+    
+    // 4 - Set up title
+    NSString *title = @"Portfolio Prices: April 23 - 27, 2012";
+    graph.title = title;
+    graph.titleTextStyle = titleStyle;
+    graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
+    graph.titleDisplacement = CGPointMake(0.0f, -16.0f);
+    
+    // 5 - Set up plot space
+    CGFloat xMin = 0.0f;
+    CGFloat xMax = [[[CPDStockPriceStore sharedInstance] datesInWeek] count];
+    CGFloat yMin = 0.0f;
+    CGFloat yMax = 800.0f;  // should determine dynamically based on max price
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
+    plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(xMin) length:CPTDecimalFromFloat(xMax)];
+    plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(yMin) length:CPTDecimalFromFloat(yMax)];
 }
 
 -(void)configurePlots {
